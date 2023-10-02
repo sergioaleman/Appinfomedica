fe503d3fdbc44eea051598df5b71da9fc2a46ffe
SERGIO ALEMAN <sergioaleman1@gmail.com>
2023-10-02 13:44:54 -0400

    Add files via upload

---
+++ pesobesidad.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Sobrepeso y Obesidad</title>
+	<meta name="description" content="Qu&eacute; son el sobrepeso y la obesidad.">
+    	<meta name="keywords" content="indice de masa corporal, imc, sobrepeso, obesidad, dieta, caloria, alimentos, peso, comida, directorio medico, especialistas, salud, grupo medico, clinica, hospital, perinatologia, medicina interna, pediatria, endocrinologia, cardiologia, dermatologia, mastologia, oftalmologia, cirugia maxilofacial, odontologia, ginecologia, obstetricia, ecografia, 4D, 3D, psicologia, psicologia clinica">
+    	<meta name="robots" content="NOINDEX,NOFOLLOW,NOARCHIVE,NOODP,NOSNIPPET">
+	<link rel="stylesheet" type="text/css" href="/imgs/styleucly.css">
+
+<style type="text/css">
+
+.framer1{
+width: 580px;
+height: 570px; 
+	padding-left:0px; 
+	padding-right:0px; 
+	padding-top:0px; 
+	padding-bottom:0px;
+background: url('images/fondogobesidad.jpg') no-repeat; 
+margin-top:0px;
+}
+
+</style>	
+
+</head>
+
+<body>
+
+<div id="contenedor" style="width: 580px; height: 570px">
+
+	<div id="cuerpo" style="width: 580px; height: 570px">
+	<div class="framer1">
+	
+	<div class="texto2">
+	<p><b><span style="font-size: 120%">¿Qu&eacute; son el sobrepeso y la obesidad?</span></b></p>
+	<p>El sobrepeso y la obesidad se definen como una acumulaci&oacute;n anormal o excesiva de grasa que puede ser perjudicial para la salud.<br><br>
+	El <b>&iacute;ndice de masa corporal (IMC)</b> es un indicador simple de la 
+relaci&oacute;n entre el peso y la talla que se utiliza frecuentemente
+ 	para identificar el sobrepeso y la obesidad en los adultos. Se calcula
+ dividiendo el peso de una persona en kilogramos por el cuadrado de su 
+talla en metros (kg/m2).<br><br>
+ 	Un IMC elevado es un importante factor de riesgo de enfermedades no transmisibles, como:
+ 
+ 	</p><ul>
+ 		<li>Las enfermedades cardiovasculares (principalmente cardiopat&iacute;a y accidente cerebrovascular)
+ 		</li><li>La diabetes
+ 		</li><li>Los trastornos del aparato locomotor (en especial la 
+osteoartritis, una enfermedad degenerativa de las articulaciones muy 
+discapacitante)
+ 		</li><li>Algunos c&aacute;nceres (del endometrio, la mama y el colon)
+ 	</li></ul>
+ 	El riesgo de contraer estas enfermedades no transmisibles crece con el aumento del IMC.
+ 	<p></p>
+	</div>
+	<div class="texto">
+	Fuente: Organizaci&oacute;n Mundial de la Salud
+	</div>
+	</div>	
+			</div>
+			
+		  </div>
+
+
+</body></html>
---
+++ psicologos_val.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html class=" js no-flexbox canvas canvastext webgl no-touch geolocation postmessage no-websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths" style=""><head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Psic&oacute;logos - Valencia</title>
+	<meta name="description" content="Psic&oacute;logos en Valencia. Su m&eacute;dico con s&oacute;lo un clic. Especialistas, Centros M&eacute;dicos, lo que usted necesita cuando de salud se trata. Planes para centros y profesionales de la salud.">
+    <meta name="keywords" content="psicologia, psicologo, psicologos en valencia, psicologos clinicos, directorio medico, especialistas, salud, centro medico, clinica, hospital, farmacia, internista, ecografista, ginecologo, odontologo, cardiologo, nutricionista, psicologo">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow">
+    <link rel="shortcut icon" href="http://www.boludo.com.ve/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="/imgs/estiloespec.css">
+	<link rel="stylesheet" href="/imgs/grids.css" type="text/css">
+	
+	<script src="/imgs/analytics.js" async=""></script><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-195121252-1', 'www.boludo.com.ve');
+  ga('send', 'pageview');
+
+</script>
+	
+
+<script type="text/javascript" src="/imgs/jquery_002.js"></script>
+
+<!-- Required Libraries -->
+ <script src="/imgs/jquery_003.js"></script>
+<script src="/imgs/jquery.js"></script>
+<script src="/imgs/modernizr.js"></script>
+
+<!-- Load Menu Specific Files -->
+<script src="/imgs/compactmenu.js"></script>
+<link rel="stylesheet" type="text/css" href="/imgs/compactmenu.css">
+
+<script>
+
+    $(function() {
+					$("#menu1").menu({
+						transition: 'inside-slide-fade-left'
+					}); 
+					 
+					$("#menu2").menu({
+						theme: 'theme-theme2'
+					}); 
+					 
+					$("#menu3").menu({
+						theme: 'theme-theme3',
+						transition: 'set3'
+					}); 
+    });
+</script>
+
+<style type="text/css">
+
+.titulo{
+	text-align: left;
+	padding-top:10px;
+	padding-left:241px;
+	padding-bottom:35px;
+	
+</style>
+
+ <!--  Google Addsense  -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2139263390076630"
+     crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+<div id="borde">
+<div id="contenedor" style="width: 903; height: 990px">
+	<div id="cabecera">
+	<div class="yui-gb">
+
+        <div class="yui-u first" style="width: 75px">
+        <a href="http://www.boludo.com.ve/directorio-medico.php">
+        <img src="/images/logo-infomedica.png" border="0"></a>
+        </div>
+        
+     
+        
+			</div>
+
+</div>
+
+	
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2139263390076630"
+     crossorigin="anonymous"></script>
+<!-- Adaptable -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2139263390076630"
+     data-ad-slot="8044386606"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+	<div id="navegador">
+	<div class="container" style="width: 200px;">
+    <div style="width: 200px;" data-status="closed" data-transition="inside-slide-fade-left" class="compactmenu theme-default" id="menu1">
+      <div class="compactanchor">
+      <a href="#"><img src="/imgs/btn_home.png" border="0" height="32" width="78"></a>
+      </div>
+      <ul class="topul" style="margin-top: 20px; display: none;">
+    <li><a href="http://www.boludo.com.ve/directorio-medico.php">Directorio M&eacute;dico</a></li>
+    <li><a href="http://www.boludo.com.ve/cardiologo_v1.php">Cardiolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/dermatologo_v1.php">Dermatolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/ecografista_v1.php">Ecograf&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/endocrinologo_v1.php">Endocrinolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/ginecologos_val.php">Ginecolog&iacute;a Obstetricia</a></li>
+	<li><a href="http://www.boludo.com.ve/mastologo_v1.php">Mastolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/neumonologo_v1.php">Neumonolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/odontologo_v1.php">Odontolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/oftalmologo_v1.php">Oftalmolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/pediatra_v1.php">Pediatr&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/perinatologo_v1.php">Perinatolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/psicologos_val.php">Psicolog&iacute;a</a></li>
+	</ul>
+    </div>
+</div>
+
+	</div>
+
+	
+	<div id="cuerpo" style="width: 900px; height: 630px">
+	<div id="marco1" style="width: 850px; height: 630px">
+	
+	<div class="titulo">
+	<h1><img src="/imgs/psicovalencia.png" alt="Psic&oacute;logos en Valencia" border="0" height="60" width="312"></h1>
+       	</div>
+       		
+	<div class="yui-gb">
+	
+			
+        <div class="yui-u first" style="width: 365px" align="right">
+        
+<div class="marcopsico">	
+<div class="texto">
+			<b><span style="font-size: 120%">Psic. Lusmai Ortega</span><br><br>
+			<a href="http://www.boludo.com.ve/psicologo_v1.php">M&aacute;s informaci&oacute;n</a></b>
+			</div>
+			</div>
+			
+        </div>
+        <div class="yui-u" style="width: 30px">&nbsp;
+		
+			</div>
+        
+		<div class="yui-u" style="width: 360px" align="left">
+		
+<div class="marcopsico">	
+<div class="texto">
+			<b><span style="font-size: 120%">Psic. Glendy Giordano</span><br><br>
+			<a href="http://www.boludo.com.ve/psicologo_v2.php">M&aacute;s informaci&oacute;n</a></b>
+			</div>
+			</div>
+			
+			</div>
+			</div>
+	</div>
+	</div>
+	<div id="pie">
+		<p>M&oacute;vil: (0426 683 6955) - E-mail: 
+		appinfomedica@gmail.com - info@boludo.com.ve</p> 
+		<p>Copyright &copy; 2013 - <?php echo date ("Y"); ?> www.boludo.com.ve. Todos los derechos reservados</p>
+	</div>
+</div>
+</div>
+
+</body></html>
