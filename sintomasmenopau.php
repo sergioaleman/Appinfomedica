015c39fcf6f3de6907df7f8d497b3b9c9bc090cd
SERGIO ALEMAN <sergioaleman1@gmail.com>
2023-10-02 13:45:55 -0400

    Add files via upload

---
+++ sintomasmenopau.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>S&iacute;ntomas de la menopausia</title>
+	<meta name="description" content="Sintomas de la Menopausia.">
+    	<meta name="keywords" content="mujer, menopausia, sintomas de la menopausia, directorio medico, especialistas, salud, grupo medico, clinica, hospital, perinatologia, medicina interna, pediatria, endocrinologia, cardiologia, dermatologia, mastologia, oftalmologia, cirugia maxilofacial, odontologia, ginecologia, obstetricia, ecografia, 4D, 3D, psicologia, psicologia clinica">
+    	<meta name="robots" content="NOINDEX,NOFOLLOW,NOARCHIVE,NOODP,NOSNIPPET">
+	<link rel="stylesheet" type="text/css" href="/imgs/styleucly.css">
+
+<style type="text/css">
+
+.framer1{
+width: 580px;
+height: 550px; 
+	padding-left:0px; 
+	padding-right:0px; 
+	padding-top:0px; 
+	padding-bottom:0px;
+background: url('images/fondogmenopau.jpg') no-repeat; 
+margin-top:0px;
+}
+
+</style>	
+
+</head>
+
+<body>
+
+<div id="contenedor" style="width: 580px; height: 550px">
+
+	<div id="cuerpo" style="width: 580px; height: 550px">
+	<div class="framer1">
+	
+	<div class="texto2">
+	<p><b><span style="font-size: 120%">S&iacute;ntomas de la menopausia</span></b></p>
+	<p>Se puede considerar a <b>la menopausia y al climaterio</b> como un 
+fen&oacute;meno natural, por cuanto se presenta en todas las mujeres, sin 
+excepci&oacute;n a 
+	partir de mediados de la d&eacute;cada de los 40 a&ntilde;os. Lo que si no puede 
+considerarse natural es el sufrimiento que se genera 
+	con el climaterio y las enfermedades que hacen su aparici&oacute;n o se 
+incrementan con la posmenopausia. Los cambios hormonales 
+	y funcionales que se inician en el climaterio son procesos biol&oacute;gicos, 
+pero la reacci&oacute;n neurovegetativa a la involuci&oacute;n ov&aacute;rica 
+	en algunas oportunidades es tan intensa que se convierten en s&iacute;ntomas 
+que obligan a su tratamiento. Aparece la llamada crisis vasomotora, 
+	caracterizada por los sofocos (bochornos) o sofocaciones, que se 
+producen durante el climaterio debido a la p&eacute;rdida intermitente del 
+control 
+	vasomotor perif&eacute;rico, manifest&aacute;ndose en forma de oleadas de calor que 
+recorren el cuerpo y suben hasta la cabeza, asociadas a enrojecimiento 
+	de la piel del cuello y de la cara, seguido de intensa sudoraci&oacute;n y en 
+ocasiones de fuertes palpitaciones y sensaci&oacute;n de angustia. 
+	Es lo que se ha dado en llamar s&iacute;ndrome climat&eacute;rico. A esta 
+sintomatolog&iacute;a deben agregarse dos factores importantes, uno 
+psicol&oacute;gico, 
+	influido por la personalidad de la mujer y otro por el entorno que la 
+rodea.</p>
+	</div>
+	</div>	
+			</div>
+			
+		  </div>
+
+
+</body></html>
