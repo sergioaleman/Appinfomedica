f552dce82b81cb8da9eb7476ffc2aa7e5a65da93
SERGIO ALEMAN <sergioaleman1@gmail.com>
2023-10-02 13:43:26 -0400

    Add files via upload

---
+++ hipertension.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Hipertensi&oacute;n</title>
+	<meta name="description" content="Qu&eacute; es la Hipertensi&oacute;n.">
+    	<meta name="keywords" content="hipertension, tension arterial, directorio medico, especialistas, salud, grupo medico, clinica, hospital, perinatologia, medicina interna, pediatria, endocrinologia, cardiologia, dermatologia, mastologia, oftalmologia, cirugia maxilofacial, odontologia, ginecologia, obstetricia, ecografia, 4D, 3D, psicologia, psicologia clinica">
+    	<meta name="robots" content="NOINDEX,NOFOLLOW,NOARCHIVE,NOODP,NOSNIPPET">
+	<link rel="stylesheet" type="text/css" href="/imgs/styleucly.css">
+
+<style type="text/css">
+
+.framer1{
+width: 580px;
+height: 550px; 
+	padding-left:0px; 
+	padding-right:0px; 
+	padding-top:0px; 
+	padding-bottom:0px;
+background: url('images/fondoghipert.jpg') no-repeat; 
+margin-top:0px;
+}
+
+</style>	
+
+</head>
+
+<body>
+
+<div id="contenedor" style="width: 580px; height: 550px">
+
+	<div id="cuerpo" style="width: 580px; height: 550px">
+	<div class="framer1">
+	
+	<div class="texto2">
+	<p><b><span style="font-size: 120%">Hipertensi&oacute;n: ¿qu&eacute; es?</span></b></p>
+	<p>Es la elevaci&oacute;n de los niveles de presi&oacute;n arterial de forma continua
+ o sostenida. Para entenderlo mejor es importante definir la presi&oacute;n 
+arterial. El coraz&oacute;n ejerce presi&oacute;n sobre las arterias para que &eacute;stas 
+conduzcan la sangre hacia los diferentes &oacute;rganos del cuerpo humano. Esta
+ acci&oacute;n es lo que se conoce como presi&oacute;n arterial. La presi&oacute;n m&aacute;xima se 
+obtiene en cada contracci&oacute;n del coraz&oacute;n y la m&iacute;nima, con cada 
+relajaci&oacute;n. La hipertensi&oacute;n supone una mayor resistencia para el 
+coraz&oacute;n, que responde aumentando su masa muscular (hipertrofia 
+ventricular izquierda) para hacer frente a ese sobreesfuerzo. Propicia 
+la arterioesclerosis (ac&uacute;mulos de colesterol en las arterias) y 
+fen&oacute;menos de trombosis (pueden producir infarto de miocardio o infarto 
+cerebral). La hipertensi&oacute;n arterial no produce s&iacute;ntomas y puede pasar 
+inadvertida.
+	<br><br>El diagn&oacute;stico se basa en un sencillo procedimiento de 
+medici&oacute;n, aunque en algunos casos son necesarias otras pruebas como el 
+holter de presi&oacute;n arterial. Es imprescindible completar el estudio con 
+un an&aacute;lisis de laboratorio (de sangre y orina) y un electrocardiograma.
+	<br><br>La hipertensi&oacute;n tiene tratamiento, un diagn&oacute;stico a tiempo y un
+ control m&eacute;dico adecuado, podr&aacute;n evitar los da&ntilde;os que &eacute;sta puede 
+producir en el organismo.</p>
+	</div>
+	<div class="texto">
+	Fuente: www.fundaciondelcorazon.com
+	</div>
+	</div>	
+			</div>
+			
+		  </div>
+
+
+</body></html>
---
+++ odontologo_v1.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html class=" js no-flexbox canvas canvastext webgl no-touch geolocation postmessage no-websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths" style=""><head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Unidad de Odontolog&iacute;a</title>
+	<meta name="description" content="Unidad de Odontolog&iacute;a. Grupo M&eacute;dico Docente Dr. Jos&eacute; V&aacute;zquez. Odontolog&iacute;a General. Odontolog&iacute;a Pedi&aacmute;:trica. Endodoncia. Ortodoncia. Pr&oacutue;tesis. Cirug&iacute;a Maxilofacial.">
+    <meta name="keywords" content="odontologia, odontologos, odontologos en valencia, odontologo, cirugia maxilofacial, odontologia pediatrica, ondodoncia, ortodoncia, protesis dental, directorio medico, especialistas, salud, grupo medico, clinica, hospital, perinatologia, medicina interna, pediatria, endocrinologia, cardiologia, dermatologia, mastologia, oftalmologia, cirugia maxilofacial, odontologia, ginecologia, obstetricia, ecografia, 4D, 3D, psicologia, psicologia clinica, uclym, mujer, menopausia, climaterio">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow">
+    <link rel="shortcut icon" href="http://www.boludo.com.ve/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="/imgs/estilomedic1.css">
+	<link rel="stylesheet" href="/imgs/grids.css" type="text/css">
+	
+	<script src="/imgs/analytics.js" async=""></script><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-195121252-1', 'www.boludo.com.ve');
+  ga('send', 'pageview');
+
+</script>
+	
+<script type="text/javascript" src="/imgs/jquery_002.js"></script>
+
+<!-- Required Libraries -->
+ <script src="/imgs/jquery_003.js"></script>
+<script src="/imgs/jquery.js"></script>
+<script src="/imgs/modernizr.js"></script>
+
+<!-- Load Menu Specific Files -->
+<script src="/imgs/compactmenu.js"></script>
+<link rel="stylesheet" type="text/css" href="/imgs/compactmenu.css">
+
+<script>
+
+    $(function() {
+					$("#menu1").menu({
+						transition: 'inside-slide-fade-left'
+					}); 
+					 
+					$("#menu2").menu({
+						theme: 'theme-theme2'
+					}); 
+					 
+					$("#menu3").menu({
+						theme: 'theme-theme3',
+						transition: 'set3'
+					}); 
+    });
+</script>
+
+<style type="text/css">
+
+.descrip2{
+	color : #666666;
+	font:100% verdana;
+	text-align: center;
+	padding-bottom:12px;
+	padding-left:0px;
+}
+
+.phone{
+	color : #000000;
+	font: bold 100% verdana;
+	text-align: center;
+	padding: 15px 0px 0px 0px;
+}
+
+.fondodon{
+	width: 247px;
+	float: left;
+	height: 423px;
+	padding-top:30px;
+	margin-bottom:10px;
+}
+
+</style>
+
+ <!--  Google Addsense  -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2139263390076630"
+     crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+<div id="borde">
+<div id="contenedor" style="width: 903px; height: 1290px">
+	<div id="cabecera">
+	<div class="yui-gb">
+
+        <div class="yui-u first" style="width: 75px">
+        <a href="http://www.boludo.com.ve/directorio-medico.php">
+        <img src="/images/logo-infomedica.png" border="0"></a>
+        </div>
+        
+       <!-- <div class="yui-u first" style="width: 75px">
+        <a href="http://www.boludo.com.ve/">
+        <img src="/imgs/cabecer2.jpg" border="0" height="180" width="75"></a>
+        </div>-->
+        
+      
+        
+			</div>
+		</div>
+		
+	
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2139263390076630"
+     crossorigin="anonymous"></script>
+<!-- Adaptable -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2139263390076630"
+     data-ad-slot="8044386606"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>		
+				
+		
+	<div id="navegador">
+	<div class="container" style="width: 200px;">
+    <div style="width: 200px;" data-status="closed" data-transition="inside-slide-fade-left" class="compactmenu theme-default" id="menu1">
+      <div class="compactanchor">
+      <a href="#"><img src="/imgs/btn_home.png" border="0" height="32" width="78"></a>
+      </div>
+
+      <ul class="topul" style="margin-top: 20px; display: none;">
+    <li><a href="http://www.boludo.com.ve/directorio-medico.php">Directorio M&eacute;dico</a></li>
+    <li><a href="http://www.boludo.com.ve/cardiologo_v1.php">Cardiolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/dermatologo_v1.php">Dermatolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/ecografista_v1.php">Ecograf&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/endocrinologo_v1.php">Endocrinolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/ginecologos_val.php">Ginecolog&iacute;a Obstetricia</a></li>
+	<li><a href="http://www.boludo.com.ve/mastologo_v1.php">Mastolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/neumonologo_v1.php">Neumonolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/odontologo_v1.php">Odontolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/oftalmologo_v1.php">Oftalmolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/pediatra_v1.php">Pediatr&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/perinatologo_v1.php">Perinatolog&iacute;a</a></li>
+	<li><a href="http://www.boludo.com.ve/psicologos_val.php">Psicolog&iacute;a</a></li>
+	</ul>
+    </div>
+</div>
+</div>		
+	<div id="marco">
+	<div class="yui-gb">
+
+        <div class="yui-u first" style="width: 185px">
+        <img src="/imgs/odon.png" alt="Odont&oacute;logos" border="0" height="60" width="60">
+        </div>
+		<div class="yui-u" style="width: 498px">
+        <h1><img src="/imgs/odonval1.png" alt="Unidad de Odontolog&iacute;a" border="0" height="60" width="393"></h1>
+        </div>
+        <div class="yui-u" style="width: 107px">
+        <img src="/imgs/gmdlogo.png" alt="Grupo M&eacute;dico Docente Dr. Jos&eacute; V&aacute;zquez" border="0" height="60" width="105">
+        </div>
+        </div>
+	</div>
+	
+	<div id="cuerpo" style="width: 500px; height: 640px">
+	<div class="yui-gb">
+        <div class="yui-u first" style="width: 220px">
+			<div class="yui-u first" style="width: 73px">
+        <img src="/imgs/gmddrjv1.jpg" border="0" height="169" width="73">
+        </div>
+        
+        <div class="yui-u first" style="width: 73px">
+        <img src="/imgs/gmddrjv2.jpg" border="0" height="169" width="73">
+        </div>
+        
+        <div class="yui-u first" style="width: 73px">
+        <img src="/imgs/gmddrjv3.jpg" border="0" height="169" width="73">  
+        </div>
+        	
+        <div class="yui-u" style="width: 200px">
+        <div class="ubic">
+        Consultorio<br>Grupo M&eacute;dico Docente<br>Dr. Jos&eacute; V&aacute;zquez<br><br><img src="/imgs/indicad1.gif" border="0" height="21" width="13">Direcci&oacute;n: Calle L&oacute;pez Latouche, N?. 130-A-26, Frente al Colegio Calasanz, Diagonal a la Cruz Roja<br>Camoruco, Valencia<br><br>
+        </div>
+        <br>
+        
+        </div>							
+	</div>
+
+		<div class="yui-u" style="width: 259px" "height:="" 430x"="">
+		<div class="fondodon">
+		<div class="descrip2">
+			<b>Dra. M? Elizabeth Polanco</b><br>
+			<span style="font-size: 90%"><i>Odontolog&iacute;a General</i></span>
+			</div>
+			<div class="descrip2">
+			<b>Dra. Yurley Galindo</b><br>
+			<span style="font-size: 90%"><i>Odontolog&iacute;a General</i></span>
+			</div>
+			<div class="descrip2">
+			<b>Dra. Mairelys Chirinos</b><br>
+			<span style="font-size: 90%"><i>Odontolog&iacute;a General</i></span>
+			</div>
+			<div class="descrip2">
+			<b>Dra. Adriana Berm&uacute;dez</b><br>
+			<span style="font-size: 90%"><i>Endodoncia </i></span>
+			</div>
+			<div class="descrip2">
+			<b>Dr. Luis Lenzurdy</b><br>
+			<span style="font-size: 90%"><i>Ortodoncia</i></span>
+			</div>
+			<div class="descrip2">
+			<b>Dra. Marjorie S&aacute;nchez</b><br>
+			<span style="font-size: 90%"><i>Ortodoncia</i></span>
+			</div>
+			<div class="descrip2">
+			<b>Dr. Manuel Su&aacute;rez</b><br>
+			<span style="font-size: 90%"><i>Protesis</i></span>
+			</div>
+			<div class="descrip2">
+			<b>Dr. Alvaro D&iacute;az</b><br>
+			<span style="font-size: 90%"><i>Cirug&iacute;a Maxilofacial</i></span>
+			</div>
+			<div class="descrip2">
+			<b>Dra. Francilenny Gonz&aacute;lez</b><br>
+			<span style="font-size: 90%"><i>Odontopediatr&iacute;a</i></span>
+			</div>
+			</div>
+		</div>
+			<div class="framer1">
+        	<div class="yui-u first" style="width: 210px">
+				<div class="horario">
+					Horario de Consulta<br><br>
+					Lunes a Viernes<br><br>
+					De 7 a.m. a 7 p.m.
+				</div>
+				</div>
+			<div class="yui-u" style="width: 250px">	
+				<div class="phone">
+					<table align="center">
+					<tbody><tr>
+					<td>
+					<img src="/imgs/phone_icon.gif" border="0" height="43" width="44"></td>
+					<td>(0241) 822 1771<br><br>
+					(0241) 824 0547</td>
+				</tr>
+					<tr>
+					<td>
+					&nbsp;</td>
+					<td>&nbsp;</td>
+				</tr>
+				<tr>
+					<td>
+					<img src="/imgs/cel_icon.gif" border="0" height="43" width="44"></td>
+					<td>0426 544 1742</td>
+				</tr>
+				</tbody></table>
+				</div>
+			</div>
+			
+			</div>	
+
+		</div>
+		</div>
+	<div id="lateral" style="width: 330px; height: 430px">
+				<div id="otras" class="cuerpolateral">
+				<div class="yui-gb">
+        
+        <div class="yui-u first" style="width: 58px">
+        <img src="/imgs/mapaubiR1C1.jpg" border="0" height="390" width="58">
+        </div>
+        
+        <div class="yui-u first" style="width: 64px">
+        <img src="/imgs/mapaubiR1C2.jpg" border="0" height="390" width="64">
+        </div>
+        
+        <div class="yui-u first" style="width: 65px">
+        <img src="/imgs/mapaubiR1C3.jpg" border="0" height="390" width="65">  
+        </div>
+        
+        <div class="yui-u first" style="width: 64px">
+        <img src="/imgs/mapaubiR1C4.jpg" border="0" height="390" width="64">
+        </div>
+        
+        <div class="yui-u first" style="width: 59px">
+        <img src="/imgs/mapaubiR1C5.jpg" border="0" height="390" width="59">
+        </div>
+
+	</div>
+	<small><a style="color:#0000FF;text-align:left" target="_blank" href="https://www.google.co.ve/maps/place/Calle+133+Lopez+Latouche/@10.2061468,-68.0100612,17z/data=%213m1%214b1%214m2%213m1%211s0x8e805d85b94c5379:0xd3d7bb02c3fee806?hl=es">Ver mapa m&aacute;s grande</a></small>						
+						</div>
+						</div>
+	
+		
+	<div id="pie">
+		<div id="navabajo">
+		<a href="http://www.boludo.com.ve/directorio-medico.php">Inicio</a> |  
+		<a href="#">Arriba</a>
+		</div>
+		<p>M&oacute;vil: (0426 683 6955) - E-mail: 
+		appinfomedica@gmail.com - info@boludo.com.ve</p> 
+		<p>Copyright &copy; 2013 - <?php echo date ("Y"); ?> www.boludo.com.ve. Todos los derechos reservados</p>
+	</div>
+</div>
+</div>
+
+</body></html>
